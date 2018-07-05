53f0d95b44c2829acd680d34132590bddd237f07
Anton Podchuvalov <podchuvalovvl@gmail.com>
2018-07-05 16:13:46 +1000

    final commit

--- bench.cpp
+++ bench.cpp
@@ -1,74 +1,104 @@
-//#define BENCHPRESS_CONFIG_MAIN
-//#include "benchpress/benchpress.hpp"
-//#include "matrix.h"
-//#include <chrono>
-//#include <iostream>
-//#include <vector>
-//
-//using namespace std;
-//
-//Matrix mul(bool transpose, int num_threads) {
-//  Matrix A(100, 100, 10);
-//  Matrix B(100, 100, 100);
-//  return A.multiply(B, transpose, num_threads);
-//}
-//
-//BENCHMARK("Multiply without transpose. 1 thread", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(false, 1);
-//  }
-//});
-//
-//BENCHMARK("Multiply with transpose. 1 thread", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(true, 1);
-//  }
-//});
-//
-//BENCHMARK("Multiply without transpose. 2 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(false, 2);
-//  }
-//});
-//
-//BENCHMARK("Multiply with transpose. 2 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(true, 2);
-//  }
-//});
-//
-//BENCHMARK("Multiply without transpose. 3 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(false, 3);
-//  }
-//});
-//
-//BENCHMARK("Multiply with transpose. 3 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(true, 3);
-//  }
-//});
-//
-//BENCHMARK("Multiply without transpose. 5 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(false, 5);
-//  }
-//});
-//
-//BENCHMARK("Multiply with transpose. 5 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(true, 5);
-//  }
-//});
-//
-//BENCHMARK("Multiply without transpose. 10 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(false, 10);
-//  }
-//});
-//
-//BENCHMARK("Multiply with transpose. 10 threads", [](benchpress::context* ctx) {
-//  for (size_t i = 0; i < ctx->num_iterations(); ++i) {
-//    mul(true, 10);
-//  }
-//});
+#define BENCHPRESS_CONFIG_MAIN
+#include "benchpress/benchpress.hpp"
+#include "matrix.h"
+#include <chrono>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+Matrix mul(bool transpose, int num_threads) {
+  Matrix A(32, 32, 10);
+  Matrix B(32, 32, 100);
+  return A.multiply(B, transpose, num_threads);
+}
+
+BENCHMARK("Multiply without transpose. 1 thread", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 1);
+});
+
+BENCHMARK("Multiply with transpose. 1 thread", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 1);
+});
+
+BENCHMARK("Multiply without transpose. 2 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 2);
+});
+
+BENCHMARK("Multiply with transpose. 2 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 2);
+});
+
+BENCHMARK("Multiply without transpose. 3 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 3);
+});
+
+BENCHMARK("Multiply with transpose. 3 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 3);
+});
+
+BENCHMARK("Multiply without transpose. 4 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 4);
+});
+
+BENCHMARK("Multiply with transpose. 4 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 4);
+});
+
+BENCHMARK("Multiply without transpose. 5 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 5);
+});
+
+BENCHMARK("Multiply with transpose. 5 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 5);
+});
+
+BENCHMARK("Multiply without transpose. 8 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 8);
+});
+
+BENCHMARK("Multiply with transpose. 8 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 8);
+});
+
+BENCHMARK("Multiply without transpose. 10 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 10);
+});
+
+BENCHMARK("Multiply with transpose. 10 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 10);
+});
+
+BENCHMARK("Multiply without transpose. 15 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 15);
+});
+
+BENCHMARK("Multiply with transpose. 15 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 15);
+});
+
+BENCHMARK("Multiply without transpose. 20 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(false, 20);
+});
+
+BENCHMARK("Multiply with transpose. 20 threads", [](benchpress::context* ctx) {
+  for (size_t i = 0; i < ctx->num_iterations(); ++i)
+    mul(true, 20);
+});
--- matrix.cpp
+++ matrix.cpp
@@ -8,23 +8,19 @@ Matrix::Matrix(const raw_matrix& matrix) : matrix_(matrix) {
   num_rows_ = matrix_.size();
   if (!matrix_.empty()) {
     size_t num_columns = matrix_[0].size();
-    for (int i = 1; i < num_rows_; i++) {
-      if (num_columns != matrix_[i].size()) {
+    for (int i = 1; i < num_rows_; i++)
+      if (num_columns != matrix_[i].size())
         throw std::length_error("Different columns size");
-      }
-    }
     num_columns_ = num_columns;
   }
-  else {
+  else
     num_columns_ = 0;
-  }
 }
 
 Matrix::Matrix(const size_t num_rows, const size_t num_col, const double val) : num_rows_(num_rows), num_columns_(num_col) {
   matrix_ = std::vector<std::vector<double>>(num_rows_);
-  for (size_t i = 0; i < num_rows_; i++) {
+  for (size_t i = 0; i < num_rows_; i++)
     matrix_[i].resize(num_columns_, val);
-  }
 }
 
 size_t Matrix::getNumRows() const {
@@ -39,9 +35,8 @@ Matrix Matrix::getTransposeMatrix() const {
   raw_matrix transposed_matrix(this->num_columns_);
   for (int i = 0; i < this->num_columns_; i++) {
     transposed_matrix[i].resize(this->num_rows_);
-    for (int j = 0; j < this->num_rows_; j++) {
+    for (int j = 0; j < this->num_rows_; j++)
       transposed_matrix[i][j] = this->matrix_[j][i];
-    }
   }
   return Matrix(transposed_matrix);
 }
@@ -58,9 +53,8 @@ void Matrix::row_multiply(const Matrix& matrix_A, const Matrix& matrix_B, const 
 }
 
 Matrix Matrix::multiply(const Matrix& matrix_B, const bool transpose, const int num_threads) const {
-  if (this->num_columns_ != matrix_B.num_rows_) {
+  if (this->num_columns_ != matrix_B.num_rows_)
     throw std::invalid_argument("Number of columns matrix A not equal number of rows matrix B");
-  }
 
   Matrix right_matrix = (transpose) ? matrix_B.getTransposeMatrix() : matrix_B;
   raw_matrix result(this->num_rows_);
@@ -76,7 +70,7 @@ Matrix Matrix::multiply(const Matrix& matrix_B, const bool transpose, const int 
       int from = size_of_part * i;
       int to = size_of_part * (i + 1);
       to += (i == num_threads - 1) ? this->num_rows_ % num_threads : 0;
-      threads.push_back(std::thread([&]() {this->row_multiply(*this, right_matrix, transpose, from, to, result); }));
+      threads.emplace_back(&Matrix::row_multiply, this, *this, right_matrix, transpose, from, to, result);
     }
     for (int i = 0; i < threads.size(); i++)
       threads[i].join();
@@ -86,15 +80,12 @@ Matrix Matrix::multiply(const Matrix& matrix_B, const bool transpose, const int 
 }
 
 bool Matrix::operator==(const Matrix& other) const {
-  if (this->num_rows_ != other.num_rows_ || this->num_columns_ != other.num_columns_) {
+  if (this->num_rows_ != other.num_rows_ || this->num_columns_ != other.num_columns_)
     return false;
-  }
-  for (int i = 0; i < this->num_rows_; i++) {
-    for (int j = 0; j < this->num_columns_; j++) {
-      if (this->matrix_[i][j] != other.matrix_[i][j]) {
+  for (int i = 0; i < this->num_rows_; i++)
+    for (int j = 0; j < this->num_columns_; j++)
+      if (this->matrix_[i][j] != other.matrix_[i][j])
         return false;
-      }
-    }
-  }
+
   return true;
 }
